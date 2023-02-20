7214fe705ea5ee90cdf4a1ed8da9c74f5889910e
siino-xyz <b114807h@gmail.com>
2023-02-21 08:30:28 +0900

    next-auth setup done

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ["lh3.googleusercontent.com", "avatars.gitthubusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/app/auth/Nav.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Login from "./Login";
+import Logged from "./Logged";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+const Nav = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  return (
+    <nav className="flex justify-between item-center py-8">
+      <Link href={"/"}>
+        <h1 className="font-bold text-lg">送信完了</h1>
+      </Link>
+      <ul className="flex item-center gap-6">
+        {!session?.user && <Login />}
+        {session?.user && <Logged image={session.user?.image || ""} />}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
